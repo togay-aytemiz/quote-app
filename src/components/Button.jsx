aa2ce3de4ba9864bf40cd30ec22089f678c8a020
Togay Aytemiz <78206784+togay-aytemiz@users.noreply.github.com>
2024-02-21 18:56:20 +0200

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,11 @@
-const Button = () => {
+const Button = ({ onRequestNewQuote }) => {
   return (
-    <button className="sm:p-5 p-3 w-full sm:max-w-sm bg-purple-600 text-white font-semibold text-center rounded-lg shadow-md sm:mt-10 mt-5 hover:bg-purple-950 transition ease-out cursor-pointer">New Quote</button>
-  )
-}
-export default Button
+    <button
+      className="sm:p-5 p-3 w-full sm:max-w-sm bg-purple-600 text-white font-semibold text-center rounded-lg shadow-md sm:mt-10 mt-5 hover:bg-purple-950 transition ease-out cursor-pointer"
+      onClick={onRequestNewQuote}
+    >
+      New Quote
+    </button>
+  );
+};
+export default Button;

cd37273dbad82328149a1c0d9b161e51003f8b18
togay-aytemiz <togayaytemiz@gmail.com>
2024-02-21 23:23:28 +0300

    fix for the author

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,18 +1,11 @@
 const Quote = ({ quote }) => {
   const { author, text } = quote;
 
-  let updatedAuthor = quote?.author;
-  let newAuthor = updatedAuthor.split(",");
-  console.log(newAuthor[0]);
-  // console.log(updatedAuthor[0]);
-  // updatedAuthor = updatedAuthor.filter((item) => item !== "type.fit");
-  // updatedAuthor = updatedAuthor.join(", ");
-
   return (
     <div>
       <p>{text}</p>
       <hr className="my-5" />
-      <p className="font-semibold">{newAuthor[0]}</p>
+      <p className="font-semibold">{author}</p>
     </div>
   );
 };

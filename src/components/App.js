e1bf55520d848fce93d9bc875fa5b5de2b6df3e9
Togay Aytemiz <78206784+togay-aytemiz@users.noreply.github.com>
2024-02-21 18:56:01 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import Quote from "./Quote";
 import { BsFillChatQuoteFill } from "react-icons/bs";
 
 const App = () => {
+  const BASE_URL = "https://type.fit/api/quotes";
+
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState({});
+
+  useEffect(() => {
+    const getQuotes = async () => {
+      const response = await fetch(BASE_URL);
+      const data = await response.json();
+      setQuotes(data);
+    };
+    getQuotes();
+  }, []);
+
+  // console.log(quotes);
+
+  const handleRandomlySelect = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setQuote(quotes[randomIndex]);
+    console.log("Randomly selected quote:", quote);
+  };
+
   return (
     <div className="mt-20 sm:mt-40 max-w-[1000px] mx-auto relative">
       <div className="mx-5 sm:mx-10 flex flex-col gap-10">
@@ -11,8 +34,8 @@ const App = () => {
           size={150}
         />
 
-        <Header />
-        <Quote />
+        <Header onRequestNewQuote={handleRandomlySelect} />
+        <Quote quote={quote} />
       </div>
     </div>
   );
